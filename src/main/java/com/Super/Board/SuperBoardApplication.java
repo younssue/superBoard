43875d3664ec6e4c0fc3843557648642858b4173
younssue <love_rvd7@naver.com>
2023-09-18 14:27:43 +0900

    Initial commit

---
+++ src/main/java/com/Super/Board/SuperBoardApplication.java
@@ -0,0 +1,13 @@
+package com.Super.Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Super/Board/SuperBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Super.Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
